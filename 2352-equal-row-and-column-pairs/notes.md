44e776f3adf9b71631b5d2be48349b87e9d0b546
Bi1ku <2008owenshi@gmail.com>
2026-08-06 22:47:55 -0400

    update leetcode 2352

--- 2352-equal-row-and-column-pairs/notes.md
+++ 2352-equal-row-and-column-pairs/notes.md
@@ -2,3 +2,5 @@
     - distinct keys only, if you attempt to add a nondistinct key... it just won't do anything
 - cannot have lists as keys or inside sets because they are mutable
     - must convert into a tuple (which are immuateable after you create them, i.e., cannot add or remove elements)
+
+to get a column from a 2-dimensional array, for i in grid[0]: column = [row[i] for row in grid]
--- 2352-equal-row-and-column-pairs/solution.py
+++ 2352-equal-row-and-column-pairs/solution.py
@@ -1,14 +1,30 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        columns = set()
+        columns = {}
         for  i in range(len(grid)):
-            columns.add(tuple([row[i] for row in grid]))
+            key = tuple([row[i] for row in grid])
+            if key in columns: columns[key] += 1
+            else: columns[key] = 1
 
         pairs = 0
         for row in grid:
-            if tuple(row) in columns: pairs += 1
+            if tuple(row) in columns:
+                pairs += columns[tuple(row)]
         
         return pairs
 
         # Runtime: O(n^2)
         # Spacetime: O(n^2)
+
+'''
+[3, 1, 2, 2]
+[1, 4, 4, 4]
+[2, 4, 2, 2]
+[2, 5, 2, 2]
+
+1: [3, 1, 2, 2]
+2: [2, 4, 2, 2]
+3:  [2, 4, 2, 2]
+
+^ Double column edge case... needed to convert from set to hashmap
+'''
